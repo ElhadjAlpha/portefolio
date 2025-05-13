0d13b74520cee1560395feb559cafe9030d7372c
ElhadjAlpha <elhadjalphaibrahimab@gmail.com>
2025-05-13 21:13:19 +0000

    Premier commit

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -143,8 +143,12 @@ EMAIL_USE_TLS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # ou os.path.join(BASE_DIR, 'static') si tu n'utilises pas Path
+]
 
 LOGIN_REDIRECT_URL = 'posts:index'
 
